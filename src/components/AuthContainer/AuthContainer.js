901c01f56acbe8b1157a97936a483a04e6f2daad
lokesh-8236 <lokesh-8236@lokesh-8236.csez.zohocorpin.com>
2020-02-17 23:05:58 +0530

    Authentication Changes

--- src/components/AuthContainer/AuthContainer.js
+++ src/components/AuthContainer/AuthContainer.js
@@ -4,7 +4,10 @@ import ThirdPartyAuthentication from './ThirdPartyAuthentication'
 class AuthContainer extends Component{
 
      state = {
-        isLoginOpen: true
+        isLoginOpen: true,
+        userName : undefined,
+        passWord : undefined,
+        emailId : undefined
       };
 
     toggleAuthContainer=()=>{
@@ -15,6 +18,18 @@ class AuthContainer extends Component{
     submitLogin=(e)=>{
         console.log(e);
     }
+
+   onUserNameChange= (event) => {
+        console.log(event.target.value);
+        this.setState({userName: event.target.value});
+    }
+    onEmailIdChange= (event) => {
+        console.log(event.target.value);
+        this.setState({emailId: event.target.value});
+    }
+    onPasswordChange= (event) => {
+        this.setState({passWord: event.target.value});
+    }
     render(){
         return(
             <div className="auth-container">
@@ -23,7 +38,16 @@ class AuthContainer extends Component{
                       <span className="nav-pill" onClick={this.toggleAuthContainer}>Sign up</span>
                 </div>
                 <div className='form-container'>
-                   { this.state.isLoginOpen? <LoginContainer submitLogin={this.submitLogin}/> : <SignUpContainer submitLogin={this.submitLogin}/>}
+                   { this.state.isLoginOpen? 
+                    <LoginContainer 
+                            submitLogin={this.submitLogin}
+                            onEmailIdChange={this.onEmailIdChange}
+                            onPasswordChange={this.onPasswordChange}/> : 
+                    <SignUpContainer
+                            submitLogin={this.submitLogin}
+                            onEmailIdChange={this.onEmailIdChange}
+                            onPasswordChange={this.onPasswordChange}
+                            onUserNameChange={this.onUserNameChange}/>}
                 </div>
                 <ThirdPartyAuthentication/>
             </div>
@@ -32,47 +56,71 @@ class AuthContainer extends Component{
     }
 }
 
-const LoginContainer =props=> {
-    
+const LoginContainer =(props)=> {
+
      return(   <div className="inner-container">
             <div className='box'>
                 <div className='input-group'>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name="username" className="login-input" placeholder="Username" onChange/>
+                    <label htmlFor="emailid">Email Id</label>
+                    <input 
+                        type="email" name="emailid"
+                        className="login-input"
+                        placeholder="Email Id "
+                        onChange={props.onEmailIdChange}/>
                 </div>
 
 
                 <div className='input-group'>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" className="login-input" placeholder="Password"/>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        className="login-input" 
+                        placeholder="Password"
+                        onChange={props.onPasswordChange}/>
                 </div>
 
-                <button  type='button' className='login-btn' onClick={()=>props.submitLogin(this)}>Login</button> 
+                <button  type='button' className='login-btn' onClick={props.submitLogin}>Login</button> 
             </div>
-        </div>);
+        </div>
+     );
     
 }
 
-const SignUpContainer = props=> {
+const SignUpContainer =(props)=> {
 
        return ( <div className="inner-container">
             <div className='box'>
                 <div className='input-group'>
                     <label htmlFor="username">Username</label>
-                    <input type="text" name="username" className="login-input" placeholder="Username"/>
+                    <input 
+                        type="text" 
+                        name="username"
+                        className="login-input" 
+                        placeholder="Username"
+                        onChange={props.onUserNameChange}/>
                 </div>
 
                 <div className='input-group'>
                     <label htmlFor="emailid">Email Id</label>
-                    <input type="email" name="emailid" className="login-input" placeholder="Email Id"/>
+                    <input 
+                        type="email" name="emailid"
+                        className="login-input"
+                        placeholder="Email Id "
+                        onChange={props.onEmailIdChange}/>
                 </div>
 
                 <div className='input-group'>
                     <label htmlFor="password">Password</label>
-                    <input type="password" name="password" className="login-input" placeholder="Password"/>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        className="login-input" 
+                        placeholder="Password"
+                        onChange={props.onPasswordChange}/>
                 </div>
 
-                <button  type='button' className='login-btn' onClick={()=>props.submitLogin(this)}> Sign Up</button> 
+                <button  type='button' className='login-btn' onClick={props.submitLogin}> Sign Up</button> 
             </div>
         </div>);
 }
